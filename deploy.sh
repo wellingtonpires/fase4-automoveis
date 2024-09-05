a9202eef9aefaf665c68b106ca76257cbd353729
Wellington de Souza Pires <wellington4046@gmail.com>
2024-09-05 19:54:50 -0300

    Teste pull request, requisito sub fase 3

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,7 @@ minikube cache delete
 minikube start --ports=8080:8080
 sudo chmod 777 /var/run/docker.sock
 minikube addons enable metrics-server
+minikube addons enable ingress
 kubectl apply -f fase3automoveis-deployment.yaml,fase3automoveis-service.yaml,postgres-initdb-config.yaml,postgres-claim0-persistentvolumeclaim.yaml,postgres-deployment.yaml,postgres-service.yaml
 kubectl autoscale deployment fase3automoveis --cpu-percent=80 --min=1 --max=10
 kubectl autoscale deployment postgres --cpu-percent=80 --min=1 --max=10
